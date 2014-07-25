2068ef6d760747d356ce211f4b4d06bb62dfdd1c
Michael <michael@vel0city.net>
2014-07-24 21:15:35 -0500

    Changed up parsing to look for commas instead of newline characters along with resetting the value in getWord for the second loop.

--- catchphrase/catchphrase.ino
+++ catchphrase/catchphrase.ino
@@ -37,6 +37,7 @@ String currentWord;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM);
 
 void setup() {
+  Serial.begin(9600);
   pinMode(PIN_SD_CS,OUTPUT);
   digitalWrite(PIN_SD_CS,HIGH);
 
@@ -63,9 +64,11 @@ void loop() {
   // Get a point and see if anyone touched the screen
   Point p = ts.getPoint();
   if (p.z > 20) {
+    Serial.println("Ooooh! That tickles!");
     // Someone touched! Get a new word, clear the screen, and draw the new one!
     currentWord = getWord();
     char* drawWord;
+    Serial.println(drawWord);
     currentWord.toCharArray(drawWord, currentWord.length());
     Tft.drawRectangle(10,110,200,60,BLACK);
     Tft.drawString(drawWord,20,110,3,WHITE);
@@ -73,26 +76,32 @@ void loop() {
 }
 
 String getWord(void) {
+  Serial.println("Opening text file for input...");
   textFile = SD.open("NAMES.txt");
   
   // Go to a random spot in the file
   textFile.seek(random(textFile.size()));
   char value = ' ';
+  Serial.print("Starting off reading a : ");
+  Serial.println(value);
   
   // Keep reading until you hit a newline or the end of the 
-  while(&value != "\n" && textFile.available()) {
+  while(&value != ',' && textFile.available()) {
     value = textFile.read();
+    Serial.println(value);
   }
-  
+  Serial.println("Found the start of a new word! Reading that word...");
   // Read in the whole line as the word
   String newWord;
   int i = 0;
-  while(&value != "\n" && i < 20 && textFile.available())
+  value = ' ';
+  while(&value != ',' && i < 20 && textFile.available())
   {
     value = textFile.read();
-    newWord.concat(value);
+    Serial.print(value);
+    newWord.concat(&value);
     i++;
   }
-  
+  Serial.println(newWord);
   return newWord;
 }
