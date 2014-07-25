b15ed6444ac96ea2f9902c0a1998ed9e201ea27d
Michael <michael@vel0city.net>
2014-07-25 00:56:07 -0500

    Now drawing all words to the middle of the screen. Also fixed bug where it would show a blank due to hitting the end of file before finding a new word.

--- catchphrase/catchphrase.ino
+++ catchphrase/catchphrase.ino
@@ -4,7 +4,6 @@
 #include <SeeedTouchScreen.h>
 
 // Boilerplate touchscreen defs
-
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) // mega
 #define YP A2   // must be an analog pin, use "An" notation!
 #define XM A1   // must be an analog pin, use "An" notation!
@@ -30,33 +29,35 @@
 #define TS_MINY 83*2
 #define TS_MAXY 913*2
 
-const int PIN_SD_CS = 4;                        // pin of sd card
+const int PIN_SD_CS = 4;   // pin of sd card
 File textFile;
 String currentWord;
 
 TouchScreen ts = TouchScreen(XP, YP, XM, YM);
 
 void setup() {
+  // Enable serial for debugging
   Serial.begin(9600);
+
+  // Init SD Card
   pinMode(PIN_SD_CS,OUTPUT);
   digitalWrite(PIN_SD_CS,HIGH);
-
-  Tft.TFTinit();
-
   Sd2Card card;
   card.init(SPI_FULL_SPEED, PIN_SD_CS);
-    
+
   if(!SD.begin(PIN_SD_CS))              
   { 
     Serial.println("failed!");
     while(1);       // init fail, die here
   }
-    
   Serial.println("SD OK!");
-  
+
+  // For better random number generator,
+  // seed noise from unconnected pin
   randomSeed(analogRead(0));
 
-  // turn on the backlight
+  // init screen and enable backlight
+  Tft.TFTinit();
   TFT_BL_ON;
 }
 
@@ -64,44 +65,64 @@ void loop() {
   // Get a point and see if anyone touched the screen
   Point p = ts.getPoint();
   if (p.z > 20) {
-    Serial.println("Ooooh! That tickles!");
-    // Someone touched! Get a new word, clear the screen, and draw the new one!
-    currentWord = getWord();
-    char* drawWord;
-    Serial.println(drawWord);
-    currentWord.toCharArray(drawWord, currentWord.length());
-    Tft.drawRectangle(10,110,200,60,BLACK);
-    Tft.drawString(drawWord,20,110,3,WHITE);
+    drawNewWord();
   }
+}
+
+void drawNewWord(void)
+{
+  Serial.println("Ooooh! That tickles!");
+  // Someone touched! Get a new word, clear the screen, and draw the new one!
+  currentWord = getWord();
+  char drawWord[22];
+  int length = currentWord.length();
+  currentWord.toCharArray(drawWord, length);
+  Serial.print("The word to draw:");
+  Serial.println(currentWord);
+
+  // Calculate the draw width from the midpoint of the screen
+  int width = length * 11;
+  // Calculate draw starting point
+  int startPoint = 120 - (width / 2);
+
+  // drawRectangle(INT16U poX, INT16U poY, INT16U length,INT16U width,INT16U color);
+  Tft.fillRectangle(0,110,240,14,BLACK);
+  Tft.fillRectangle(startPoint,110,(width -2),16,BLACK);
+  Tft.drawString(drawWord,startPoint,110,2,RED);
 }
 
 String getWord(void) {
   Serial.println("Opening text file for input...");
-  textFile = SD.open("NAMES.txt");
-  
-  // Go to a random spot in the file
-  textFile.seek(random(textFile.size()));
+
+  // Keep reading until you hit a newline or the end of the file
+  // Repeat if we hit the end of the file
+  boolean hitEOF = true;
   char value = ' ';
-  Serial.print("Starting off reading a : ");
-  Serial.println(value);
-  
-  // Keep reading until you hit a newline or the end of the 
-  while(&value != ',' && textFile.available()) {
-    value = textFile.read();
-    Serial.println(value);
+  while (hitEOF) {
+    textFile = SD.open("NAMES.txt");
+    textFile.seek(random(textFile.size()));
+    while(textFile.available()) {
+      value = textFile.read();
+      if (value == ',') {
+        hitEOF = false;
+        break;
+      }
+      textFile.close();
+    }
   }
+
   Serial.println("Found the start of a new word! Reading that word...");
   // Read in the whole line as the word
   String newWord;
   int i = 0;
   value = ' ';
-  while(&value != ',' && i < 20 && textFile.available())
+  while(value != ',' && i < 20 && textFile.available())
   {
     value = textFile.read();
-    Serial.print(value);
-    newWord.concat(&value);
+    newWord.concat(value);
     i++;
   }
-  Serial.println(newWord);
+  textFile.close();
   return newWord;
 }
+
